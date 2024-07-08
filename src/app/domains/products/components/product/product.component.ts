ad483e9aca3eea784d5741d641af28a4f301aafd
Julio Bastidas <juliobas@gmail.com>
2024-07-08 17:18:15 -0400

    nuevos avances, colocando de forma dinamica la data de productos con su modelo, colocando header

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -9,14 +10,12 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ProductComponent {
   //img = 'https://picsum.photos/640/640?r=' + Math.random();
-  @Input({required: true}) img: string = '';
-  @Input() price: number = 0;
-  @Input() title: string = '';
+  @Input({required: true}) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
     console.log('click from child');
-    this.addToCart.emit('Hola este es un mensaje desde el hijo ' + this.title);
+    this.addToCart.emit('Hola este es un mensaje desde el hijo ' + this.product.title);
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,15 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=1',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 2',
+        price: 200,
+        image: 'https://picsum.photos/640/640?r=2',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 3',
+        price: 300,
+        image: 'https://picsum.photos/640/640?r=3',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 4',
+        price: 150,
+        image: 'https://picsum.photos/640/640?r=4',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 5',
+        price: 350,
+        image: 'https://picsum.photos/640/640?r=5',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Product 6',
+        price: 280,
+        image: 'https://picsum.photos/640/640?r=6',
+        creationAt: new Date().toISOString()
+      }
+    ];
+
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string) {
     console.log('from parent');
     console.log('event', event);
