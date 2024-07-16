7fd6cd2964f7e7e468f1af44144993cf64533102
Julio Bastidas <juliobas@gmail.com>
2024-07-16 14:43:10 -0400

    avances

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductComponent } from '../../components/product/product.component';
-import { HeaderComponent } from '../../../shared/components/header/header.component';
-import { Product } from '../../../shared/models/product.model';
-import { CartService } from '../../../shared/services/cart.service';
+import { ProductComponent } from '@products/components/product/product.component';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { Product } from '@shared/models/product.model';
+import { CartService } from '@shared/services/cart.service';
+import { ProductService } from '@shared/services/product.service';
 
 @Component({
   selector: 'app-list',
@@ -16,55 +17,45 @@ export class ListComponent {
 
   products = signal<Product[]>([]);
   private cartService = inject(CartService);
+  private productService = inject(ProductService);
 
-  constructor() {
-    const initProducts: Product[] = [
-      {
-        id: Date.now(),
-        title: 'Product 1',
-        price: 100,
-        image: 'https://picsum.photos/640/640?r=1',
-        creationAt: new Date().toISOString()
+  /*
+  ngOnInit() {
+    this.productService.getProducts().subscribe({
+      next: (products) => {
+        this.products.set(products);
+        const product = products[0];
+        console.log(JSON.parse(product.images[0]));
       },
-      {
-        id: Date.now(),
-        title: 'Product 2',
-        price: 200,
-        image: 'https://picsum.photos/640/640?r=2',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 3',
-        price: 300,
-        image: 'https://picsum.photos/640/640?r=3',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 4',
-        price: 150,
-        image: 'https://picsum.photos/640/640?r=4',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 5',
-        price: 350,
-        image: 'https://picsum.photos/640/640?r=5',
-        creationAt: new Date().toISOString()
-      },
-      {
-        id: Date.now(),
-        title: 'Product 6',
-        price: 280,
-        image: 'https://picsum.photos/640/640?r=6',
-        creationAt: new Date().toISOString()
+      error: (error) => {
+        console.error(error);
       }
-    ];
-
-    this.products.set(initProducts);
+    });
   }
+    */
+
+  ngOnInit() {
+    this.productService.getProducts().subscribe(res => {
+      const products = res.map(product => {
+        if (product.images && product.images.length > 0) {
+          const jsonString = product.images[0];
+          try {
+            const urlArray = JSON.parse(jsonString);
+            if (urlArray && urlArray.length > 0) {
+              product.images = urlArray;
+            }
+          }
+          catch (e) {
+            console.error('Error al parsear JSON', e);
+          }
+        }
+        return product;
+      });
+      this.products.set(products);
+    });
+  }
+
+
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, SimpleChanges, inject, signal } from '@angular/core';
-import { Product } from '../../models/product.model';
-import { CartService } from '../../services/cart.service';
+import { CartService } from '@shared/services/cart.service';
 
 @Component({
   selector: 'app-header',
